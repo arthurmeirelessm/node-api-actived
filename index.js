cf2790acf36e3f12ffcf812f36351afdf87cde78
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-06-10 01:46:24 -0300

    Add endpoints to postman test

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
+const { json } = require('express');
 var express = require('express')
 
 var server = express()
 
+server.use(express.json())
+
 //Query params = ?name=NodeJs
 //Query params = /course/2
 //Request body = { name: 'NodeJs', type: 'BackEnd' }
-const namesToTest = ['Arthur', 'Fábio', 'Dior', 'Heughy', 'Bruto']
+const specialtys = ['Cardiologist', 'Ophthalmologist', 'Pediatrician', 'Surgeon', 'Dentist']
 
-server.get('/courses/', (req, res) => {
-    const index = req.query.index
-    return res.json(namesToTest[index])
+
+server.get('/specialtys', (req, res) => {
+    return res.json(specialtys)
 })
 
+server.get('/specialtys/:index', (req, res) => {
+    const index = req.params.index
+    return res.json(specialtys[index])
+})
+
+server.post('/specialtys', (req, res) => {
+    const nameOfSpecialtysReq = req.body.name
+     specialtys.push(nameOfSpecialtysReq)
+     return res.json(specialtys)
+})
 
 server.listen(3000)
 

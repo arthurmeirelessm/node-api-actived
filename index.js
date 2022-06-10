38a7b09134d73d16c1836234e5dad6554f8aa068
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2022-06-09 23:48:15 -0300

    Testinf route for Id

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ var express = require('express')
 
 var server = express()
 
+//Query params = ?name=NodeJs
+//Query params = /course/2
+//Request body = { name: 'NodeJs', type: 'BackEnd' }
+const namesToTest = ['Arthur', 'Fábio', 'Dior', 'Heughy', 'Bruto']
 
-server.get('/courses', () => {
-    c
+server.get('/courses/', (req, res) => {
+    const index = req.query.index
+    return res.json(namesToTest[index])
 })
 
+
+server.listen(3000)
 
